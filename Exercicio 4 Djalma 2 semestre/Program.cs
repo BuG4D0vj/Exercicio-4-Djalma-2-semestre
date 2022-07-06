8458521af4daab96164232e774e7cb5f9d7c44ef
Vinícius <Vincius@DESKTOP-HLF9SUT>
2022-07-06 20:10:26 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio 4 Djalma 2 semestre/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_4_Djalma_2_semestre
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int i;
+			for (i = 1; i <= 100 / 2; i++)
+			{
+				Console.WriteLine(i * 2);
+			}
+			Console.ReadLine();
+		}
+	}
+}
